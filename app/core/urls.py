740ed8e2e175a8549805d65be0c07b39b3b69ff2
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-25 02:10:04 -0500

    Create error templates

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
@@ -11,3 +12,15 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        [
+            path("400", TemplateView.as_view(template_name="400.html")),
+            path("401", TemplateView.as_view(template_name="401.html")),
+            path("403", TemplateView.as_view(template_name="403.html")),
+            path("404", TemplateView.as_view(template_name="404.html")),
+            path("405", TemplateView.as_view(template_name="405.html")),
+            path("500", TemplateView.as_view(template_name="500.html")),
+        ]
+    )
