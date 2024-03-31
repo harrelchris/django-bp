827822d6171991ccf393cde39a6d198d4751231c
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-03-31 12:23:47 -0500

    Configure debug urls

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.views import defaults
 
 admin.site.index_title = "Site administration"
 admin.site.name = "Django Admin"
@@ -10,18 +11,16 @@ admin.site.site_title = "Django site admin"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path("admin/", admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns.extend(
         [
-            path("400", TemplateView.as_view(template_name="400.html")),
-            path("401", TemplateView.as_view(template_name="401.html")),
-            path("403", TemplateView.as_view(template_name="403.html")),
-            path("404", TemplateView.as_view(template_name="404.html")),
-            path("405", TemplateView.as_view(template_name="405.html")),
-            path("500", TemplateView.as_view(template_name="500.html")),
+            path("__debug__/", include("debug_toolbar.urls")),
+            path("400/", defaults.bad_request, kwargs={"exception": Exception("Bad Request")}),
+            path("403/", defaults.permission_denied, kwargs={"exception": Exception("Permission Denied")}),
+            path("404/", defaults.page_not_found, kwargs={"exception": Exception("Not Found")}),
+            path("500/", defaults.server_error),
         ]
     )
